19e0f17006e7e87fbc34b50270467d17fa67d413
danielzhang183 <danielzhang182@gmail.com>
2022-08-19 06:54:35 +0800

    feat: add volume to persist data

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const ronin = require('ronin-server')
 const mocks = require('ronin-mocks')
+const database = require('ronin-database')
 
 const server = ronin.server()
 
+database.connect(process.env.CONNECTIONSTRING)
 server.use('/', mocks.server(server.Router(), false, true))
 server.start()
